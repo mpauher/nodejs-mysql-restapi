64416b986927cabaf657de89c85eba8c269f24f4
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-01-26 14:21:53 -0500

    Import routes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import indexRoutes from './routes/index.routes.js'
 
 const app = express();
 
+app.use(express.json())
+
 app.use(indexRoutes)
-app.use(productsRoutes)
+app.use('/api',productsRoutes)
 
 app.listen(3000);
 console.log('Server running on port 3000');
